f862c0e536897e5063599fb74ca637de99ab96c7
FichoZee <filip.zafran@gmail.com>
2020-04-19 00:09:58 +0200

    bject Oriented JavaScript Tutorial #6 - Class Methods

--- object.js
+++ object.js
@@ -1,24 +1,20 @@
-// when we want to create a new user it will do:
-//     !! pocinje IZNUTRA PREMA VAN !!!
-// 1. creates an empty object
-// 2. sets values 'this' to the nre empty object
-// 3. calls the contstructor method
-
 class User {
-  //  constructor will be responsible to creating this new user object
-
   constructor(email, name) {
-    // 'email , name' are arguments
     this.email = email;
     this.name = name;
+  }
 
-    // when ever we create a new user with different values for the properties (email , name..)
+  // login & logout are methods
+  login() {
+    console.log(this.email, "just logged in");
+  }
+  logout() {
+    console.log(this.email, "just logged out");
   }
 }
 
-//capital U(ser) = classes are with capital (Upper Case start)
-
 var UserOne = new User("ryu@gmail.com", "Ryu");
-// 'ryu@gmail.com', 'Ryu' - arguments
-
 var UserTwo = new User("yoshi@gmail.com", "yoshi");
+
+UserOne.login();
+UserTwo.logout();
