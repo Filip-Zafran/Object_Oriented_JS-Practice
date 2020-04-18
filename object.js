02aab8c48eaec2f350133566e75d5791f1d99116
FichoZee <filip.zafran@gmail.com>
2020-04-19 00:02:20 +0200

    [200~Object Oriented JavaScript Tutorial #5 - Class Constructors

--- object.js
+++ object.js
@@ -1,13 +1,24 @@
-var userOne = {
-  email: "ryu@ninjas.com",
-  name: "Ryu",
-  login() {
-    console.log(this.email, "has logged in");
-  },
-  logout() {
-    console.log(this.email, "has logged out");
-  },
-};
+// when we want to create a new user it will do:
+//     !! pocinje IZNUTRA PREMA VAN !!!
+// 1. creates an empty object
+// 2. sets values 'this' to the nre empty object
+// 3. calls the contstructor method
 
-userOne.login();
-userOne.logout();
+class User {
+  //  constructor will be responsible to creating this new user object
+
+  constructor(email, name) {
+    // 'email , name' are arguments
+    this.email = email;
+    this.name = name;
+
+    // when ever we create a new user with different values for the properties (email , name..)
+  }
+}
+
+//capital U(ser) = classes are with capital (Upper Case start)
+
+var UserOne = new User("ryu@gmail.com", "Ryu");
+// 'ryu@gmail.com', 'Ryu' - arguments
+
+var UserTwo = new User("yoshi@gmail.com", "yoshi");
